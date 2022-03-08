0a12579613a5f109ea13c3fe67c1d9a0008c47f8
Martin Günther <martin.guenther@dfki.de>
2022-03-08 16:58:23 +0100

    nvisii: Refactor adding_mesh_object
    This is a pure refactoring and shouldn't change behavior, just remove
    the amount of duplicated code.

--- scripts/nvisii_data_gen/single_video_pybullet.py
+++ scripts/nvisii_data_gen/single_video_pybullet.py
@@ -236,123 +236,79 @@ def adding_mesh_object(name, obj_to_load, texture_to_load, scale=1, debug=False)
     # obj_to_load = toy_to_load + "/meshes/model.obj"
     # texture_to_load = toy_to_load + "/materials/textures/texture.png"
 
-    print("loading:",obj_to_load)
+    print("loading:", obj_to_load)
 
     if texture_to_load is None:
         toys = load_obj_scene(obj_to_load)
-
-        toy_parent_transform = visii.entity.get(toys[0]).get_transform().get_parent()
-        toy_parent_transform.set_scale(visii.vec3(scale))
-        toy_parent_transform.set_position(
-            visii.vec3(
-                random.uniform(0.1,2),
-                random.uniform(-1,1),
-                random.uniform(-1,1),
-                )
-            )
-        toy_parent_transform.set_rotation(
-            visii.quat(
-                random.uniform(0, 1),
-                random.uniform(0, 1),
-                random.uniform(0, 1),
-                random.uniform(0, 1),
-            )
-        )
-
+        toy_transform = visii.entity.get(toys[0]).get_transform().get_parent()
         name = toys[0]
-
-        id_pybullet = create_physics(name, mass = np.random.rand()*5)
-
-        visii_pybullet.append(
-            {
-                'visii_id':name,
-                'bullet_id':id_pybullet,
-                'base_rot':None,
-            }
-        )
-        gemPos, gemOrn = p.getBasePositionAndOrientation(id_pybullet)
-        force_rand = 10
-        object_position = 0.01
-        p.applyExternalForce(
-            id_pybullet,
-            -1,
-            [   random.uniform(-force_rand,force_rand),
-                random.uniform(-force_rand,force_rand),
-                random.uniform(-force_rand,force_rand)],
-            [   random.uniform(-object_position,object_position),
-                random.uniform(-object_position,object_position),
-                random.uniform(-object_position,object_position)],
-            flags=p.WORLD_FRAME
-        )
-
-        for entity_name in toys:
-            names_to_export.append(entity_name)
-            add_cuboid(entity_name, scale=scale, debug=debug)
-        names_to_export.append(toy_parent_transform.get_name())
-
+        names_to_export.append(toy_transform.get_name())
     else:
+        toys = [name]
+
         if obj_to_load in mesh_loaded:
             toy_mesh = mesh_loaded[obj_to_load]
         else:
-            toy_mesh = visii.mesh.create_from_file(name,obj_to_load)
+            toy_mesh = visii.mesh.create_from_file(name, obj_to_load)
             mesh_loaded[obj_to_load] = toy_mesh
 
         toy = visii.entity.create(
-            name = name,
-            transform = visii.transform.create(name),
-            mesh = toy_mesh,
-            material = visii.material.create(name)
+            name=name,
+            transform=visii.transform.create(name),
+            mesh=toy_mesh,
+            material=visii.material.create(name)
         )
 
-        toy_rgb_tex = visii.texture.create_from_file(name,texture_to_load)
+        toy_rgb_tex = visii.texture.create_from_file(name, texture_to_load)
         toy.get_material().set_base_color_texture(toy_rgb_tex)
-        toy.get_material().set_roughness(random.uniform(0.1,0.5))
+        toy.get_material().set_roughness(random.uniform(0.1, 0.5))
 
+        toy_transform = toy.get_transform()
 
-
-        toy.get_transform().set_scale(visii.vec3(scale))
-        toy.get_transform().set_position(
-            visii.vec3(
-                random.uniform(0.1,2),
-                random.uniform(-1,1),
-                random.uniform(-1,1),
-                )
-            )
-        toy.get_transform().set_rotation(
-            visii.quat(
-                random.uniform(0, 1),
-                random.uniform(0, 1),
-                random.uniform(0, 1),
-                random.uniform(0, 1),
-            )
+    toy_transform.set_scale(visii.vec3(scale))
+    toy_transform.set_position(
+        visii.vec3(
+            random.uniform(0.1, 2),
+            random.uniform(-1, 1),
+            random.uniform(-1, 1),
         )
-
-        id_pybullet = create_physics(name, mass = np.random.rand()*5)
-
-        visii_pybullet.append(
-            {
-                'visii_id':name,
-                'bullet_id':id_pybullet,
-                'base_rot':None,
-            }
+    )
+    toy_transform.set_rotation(
+        visii.quat(
+            random.uniform(0, 1),
+            random.uniform(0, 1),
+            random.uniform(0, 1),
+            random.uniform(0, 1),
         )
-        gemPos, gemOrn = p.getBasePositionAndOrientation(id_pybullet)
-        force_rand = 10
-        object_position = 0.01
-        p.applyExternalForce(
-            id_pybullet,
-            -1,
-            [   random.uniform(-force_rand,force_rand),
-                random.uniform(-force_rand,force_rand),
-                random.uniform(-force_rand,force_rand)],
-            [   random.uniform(-object_position,object_position),
-                random.uniform(-object_position,object_position),
-                random.uniform(-object_position,object_position)],
-            flags=p.WORLD_FRAME
-        )
-        names_to_export.append(name)
+    )
 
-        add_cuboid(name, scale=scale, debug=debug)
+    id_pybullet = create_physics(name, mass=(np.random.rand() * 5))
+
+    visii_pybullet.append(
+        {
+            'visii_id': name,
+            'bullet_id': id_pybullet,
+            'base_rot': None,
+        }
+    )
+    gemPos, gemOrn = p.getBasePositionAndOrientation(id_pybullet)
+    force_rand = 10
+    object_position = 0.01
+    p.applyExternalForce(
+        id_pybullet,
+        -1,
+        [random.uniform(-force_rand, force_rand),
+         random.uniform(-force_rand, force_rand),
+         random.uniform(-force_rand, force_rand)],
+        [random.uniform(-object_position, object_position),
+         random.uniform(-object_position, object_position),
+         random.uniform(-object_position, object_position)],
+        flags=p.WORLD_FRAME
+    )
+
+    for entity_name in toys:
+        names_to_export.append(entity_name)
+        add_cuboid(entity_name, scale=scale, debug=debug)
 
 google_content_folder = glob.glob(opt.objs_folder_distrators + "*/")
 
