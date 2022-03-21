nvisii: Fix 2d bounding box computation
This fixes the computed bounding_box_minx_maxx_miny_maxy when
height != width.

scripts/nvisii_data_gen/single_video_pybullet.py
@@ -524,7 +524,7 @@ while True:
             options="entity_id",
             file_path = f"{opt.outf}/{str(i_render).zfill(5)}.seg.exr"
         )
-        segmentation_array = visii.render_data(
+        segmentation_mask = visii.render_data(
             width=int(opt.width),
             height=int(opt.height),
             start_frame=0,
@@ -532,6 +532,7 @@ while True:
             bounce=int(0),
             options="entity_id",
         )
+        segmentation_mask = np.array(segmentation_mask).reshape((opt.height, opt.width, 4))[:, :, 0]
         export_to_ndds_file(
             f"{opt.outf}/{str(i_render).zfill(5)}.json",
             obj_names = names_to_export,
@@ -540,7 +541,7 @@ while True:
             camera_name = 'camera',
             # cuboids = cuboids,
             camera_struct = random_camera_movement,
-            segmentation_mask = np.array(segmentation_array).reshape(opt.width,opt.height,4)[:,:,0],
+            segmentation_mask=segmentation_mask,
             compute_visibility_fraction=(not opt.no_visibility_fraction),
         )
         visii.render_data_to_file(

scripts/nvisii_data_gen/utils.py
@@ -1023,7 +1023,7 @@ def export_to_ndds_file(
             bounce=int(0),
             options="entity_id",
         )
-        segmentation_mask = np.array(segmentation_mask).reshape(width, height, 4)[:, :, 0]
+        segmentation_mask = np.array(segmentation_mask).reshape((height, width, 4))[:, :, 0]
 
     visible_object_ids = np.unique(segmentation_mask.astype(int))
 
@@ -1152,7 +1152,7 @@ def export_to_ndds_file(
                 bounce=int(0),
                 options="entity_id",
             )
-            segmentation_unique_mask = np.array(segmentation_unique_mask).reshape(width,height,4)[:,:,0]
+            segmentation_unique_mask = np.array(segmentation_unique_mask).reshape((height, width, 4))[:, :, 0]
 
             px_count_visib = len(np.where(segmentation_mask == int(id_keys_map[obj_name]))[0])
             px_count_all = len(np.where(segmentation_unique_mask == int(id_keys_map[obj_name]))[0])