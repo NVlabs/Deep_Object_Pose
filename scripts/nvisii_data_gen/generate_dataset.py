538d12bf1aef17d3f094eaefe4f82d46d69b0575
Jonathan Tremblay <you@example.com>
2022-07-25 10:10:08 -0700

    removing some mistakes

--- scripts/nvisii_data_gen/generate_dataset.py
+++ scripts/nvisii_data_gen/generate_dataset.py
@@ -5,19 +5,13 @@ import subprocess
 
 # 20 000 images
 
-for i in range(97, 100):
+for i in range(0, 100):
 	to_call = [
 		"python",'single_video_pybullet.py',
 		'--spp','10',
 		'--nb_frames', '200',
-		'--nb_distractors','10',
-		'--nb_objects',str(int(random.uniform(2,5))),
-		'--scale', '0.015',
-		'--skyboxes_folder','/home/jtremblay/code/visii_dr/dome_hdri_haven/',
-		"--skip_frame",'100',
-		'--objs_folder_distrators','/media/jtremblay/data_large/google_scanned/google_scanned_models/',
-		'--path_single_obj','/home/jtremblay/code/visii_dr/content/models/grocery_ycb/003_cracker_box/google_16k/textured.obj',
-		'--outf',f"andrew_ycb_dope/{str(i).zfill(3)}",
+		'--nb_objects',str(int(random.uniform(50,75))),
+		'--scale', '0.01',
+		'--outf',f"dataset/{str(i).zfill(3)}",
 	]
 	subprocess.call(to_call)
-	# raise()
