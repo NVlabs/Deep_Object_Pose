0288c145c52cdd70b392b2a374b4a36885f15abc
Marco19987 <marco1998.mds@gmail.com>
2024-05-07 09:40:18 +0200

    add docker folder, to finish

---
+++ docker/init_workspace.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+
+# Copyright (c) 2018, NVIDIA CORPORATION. All rights reserved.
+# Full license terms provided in LICENSE.md file.
+
+# Stop in case of any error.
+set -e
+
+source /opt/ros/humble/setup.bash
+
+# Create colcon workspace.
+mkdir -p ${ROS2WS}/src
+cd ${ROS2WS}/
+colcon_build
